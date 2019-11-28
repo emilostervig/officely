28b2635d90010e99fd7d5c6152bb2ed0a32fb732
Emil <emil.ostervig2@gmail.com>
2019-11-28 09:13:21 +0100

    Build now in git files

---
+++ build/precache-manifest.8fae4da748994c1c8b7d67dd391c586c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c45b4ee92b943ed08899a63c22761aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a989dc949d5fab55f9",
+    "url": "/static/css/2.9b39ae8c.chunk.css"
+  },
+  {
+    "revision": "42a989dc949d5fab55f9",
+    "url": "/static/js/2.8d29eff4.chunk.js"
+  },
+  {
+    "revision": "21ef4da6c42875cad7e5",
+    "url": "/static/js/main.3c527625.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
